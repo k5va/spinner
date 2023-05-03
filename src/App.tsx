51522de00535bc3ad1925a77e2705b82ab650459
k5va <k5va@yandex.ru>
2023-05-03 08:59:16 +0500

    Init

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.module.scss';
+import { Spinner } from './components/spinner/spinner';
+
+function App() {
+  return (
+    <div>
+      <Spinner />
+    </div>
+  );
+}
+
+export default App;
